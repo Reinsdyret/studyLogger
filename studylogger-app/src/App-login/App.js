506714eea47de0478b3b6e7c7f0032490fb30404
Lars Haukland <lars.haukland@gmail.com>
2023-01-30 15:18:12 +0100

    Initialized project with react Also installed react-burger-menu for a burger navigation menu

---
+++ studylogger-app/src/App-login/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the study logger</h1>
+      <p>Login or register</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ studylogger-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the studylogger</h1>
+      <p>Please login or register to continue</p>
+    </div>
+  );
+}
+
+export default App;
